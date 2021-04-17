510431b8dfcc6cd3397082db6a5336ee83bcd5ce
Gonzalo Delgado Chaves <gondelcha@alum.us.es>
2021-04-17 12:34:29 +0200

    Task-008: añade relación Manager-Task, lógica de aplicacion y vistas para listar y ver Tasks de Manager

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -4,9 +4,11 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -15,6 +17,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.DomainEntity;
+import acme.framework.entities.Manager;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -67,5 +70,10 @@ public class Task extends DomainEntity {
 	}
 
 	//Relationships
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Manager manager;
 	
 }
